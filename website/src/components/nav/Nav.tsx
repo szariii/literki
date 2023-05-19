8795da243fdb2e016471b14e7e69589cbf81350b
Konrad <konrad22.02@wp.pl>
2023-05-19 10:03:57 +0200

    Create navbar

---
+++ website/src/components/nav/Nav.tsx
@@ -0,0 +1,17 @@
+import "../../style/nav/nav.scss";
+
+const Nav = () => {
+  return (
+    <div className="navbar">
+      <h1 className="navbar__title">Literki</h1>
+
+      <div className="navbar__container">
+        <button className="navbar__button"></button>
+        <button className="navbar__button"></button>
+        <button className="navbar__button"></button>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
---
+++ website/src/pages/LoginPage.tsx
@@ -0,0 +1,7 @@
+import "../style/loginPage/loginPage.scss";
+
+const LoginPage = () => {
+  return <div></div>;
+};
+
+export default LoginPage;
