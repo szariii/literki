72cf5fe400d098a4b6d2324005dbe8d554318685
Konrad <konrad22.02@wp.pl>
2023-06-23 12:07:12 +0200

    Add redirect to main page

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,17 +1,24 @@
 //Pages
 import LoginPage from "./pages/LoginPage";
+import MainPage from "./pages/MainPage";
 
 //Components
 import Nav from "./components/nav/Nav";
 
 //Style
-import "./style/app/_app.scss"
+import "./style/app/_app.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="appContainer">
       <Nav />
-      <LoginPage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/main" element={<MainPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
--- website/src/components/loginPage/LoginComponent.tsx
+++ website/src/components/loginPage/LoginComponent.tsx
@@ -3,16 +3,29 @@ import "../../style/loginPage/_loginComponent.scss";
 import settings from "../../settings.json"
 
 import { ChangeEvent, useState } from "react";
+import { useNavigate} from "react-router-dom"
+import ErrorComponent from "../helpiongComponents/ErrorComponent";
+import WaitingComponent from "../helpiongComponents/WaitingComponent";
 
 const LoginComponent = () => {
+  const [errorMessage,setErrorMessage] = useState(false)
+  const [waiting,setWaiting] = useState(false)
+  const navigate = useNavigate()
   const [loginData, setLoginData] = useState<LoginData>({
     nick: "",
     password: "",
   });
 
   const loginButtonHandler=async()=>{
+    setWaiting(!waiting)
     const data = await axios.post(`${settings.address}/login`, loginData)
-    console.log(data)
+    console.log(data.data.success)
+    setWaiting(!waiting)
+    if(data.data.success){
+      navigate("/main")
+    }else{
+      setErrorMessage(!errorMessage)
+    }
   }
 
   return (
@@ -48,6 +61,8 @@ const LoginComponent = () => {
         </div>
       </div>
       <button onClick={loginButtonHandler} className="loginButton">Zaloguj się</button>
+      {errorMessage?<ErrorComponent header="Błędne dane" onClickHandler={()=>{setErrorMessage(!errorMessage)}} />:""}
+      {waiting?<WaitingComponent header="logowanie..." />:""}
     </>
   );
 };
